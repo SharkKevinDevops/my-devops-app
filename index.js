e870a8e6c721ec7b9721a63e07ad263e1e6c94bc
SharkKevinDevops <phuccloud86@gmail.com>
2025-03-11 17:18:55 +0700

    Fix ReferenceError: result is not defined

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => result.send('Hello World!'));
+app.get('/', (req, res) => {
+  const result = 'Hello, World!';  // Define the result
+  res.send(result);
+});
 
 app.listen(3000, () => console.log('Server running on port 3000'));
